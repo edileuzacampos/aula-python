1501711f2bf8df53c585640970871461ecfc9eac
edileuzacampos <edileuzacampos720@gmail.com>
2026-07-15 19:25:51 -0300

    first commit

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+preco = float(input('Digite o preço do produto: '))
+percentual = float(input('Digite o percentual de desconto (0-100%): '))
+
+desconto = preco * (percentual / 100)
+final = preco - desconto
+
+print(f'O preço do produto é {preco}. Desconto de {percentual}%')
+print(f'O valor calculado de desconto: {desconto}. O valor final do produto: {final}' )
